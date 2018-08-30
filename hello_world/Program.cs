13212095623931b987e45d4b6c83cbe0b7b3bf4b
Anastasiia <anastasiia@Trues-MacBook-Air.local>
2018-08-30 14:06:32 +0300

    first commit

---
+++ hello_world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_world
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, input, your name, please");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}!", name);
+        }
+    }
+}
